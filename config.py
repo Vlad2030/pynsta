3bbb98aa8775cd5f33399920d8e42f1e7a5d0852
Vlad2030 <61238982+Vlad2030@users.noreply.github.com>
2021-05-25 22:11:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+# API.
+# Take from https://my.telegram.org/apps.
+api_id = 4210896 # API ID.
+api_hash = 'aa967870c25b76ad4af8eac0cc8e117e' # API hash.
+
+# Count.
+i = 0
+
+# Chat's ID.
+inst_chat_id_1 = -1001167624782 # 23 524 members.
+inst_chat_id_2 = -1001274465624 # 29 131 members.
+
+# Links.
+inst_link = 'https://instagram.com/_vladdd.0 \n' # Your profile.
+inst_link1 = 'https://www.instagram.com/p/COxZ6X5h_Zv/ \n' # Your post.
+
+# Text.
+inst_text = ('💎Взаимная подписка💎 \n✅Подпишусь с 3-ех аккаунтов в течении часа✅ \n') # Your text.
+
+# Succsesful message (if message have been send).
+cum = 'Succsesful'
+
+# Ad.
+ad = '\n за авто-продвижением в лс' # Your ad text.
+
+# something.
+m = 'min.'
+dd = ':'
+d = '.'
+n = '\n'
